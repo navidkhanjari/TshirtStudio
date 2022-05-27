ab37b917edb8029b58ed4ab21fc0313feeff87c1
navid khanjari <navidkhanjari72@gmail.com>
2022-05-27 18:41:20 +0430

    Add project files.

---
+++ TshirtStudio.DataLayer/Entities/MainSlider.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TshirtStudio.DataLayer.Entities
+{
+   public class MainSlider
+    {
+        [Key]
+        public int Sliderid { get; set; }
+
+        [Display(Name = "عنوان اسلایدر")]
+        [Required(ErrorMessage = "وارد کردن {0} اجباری است .")]
+        public string SliderTitle { get; set; }
+
+        [Display(Name = "الت اسلایدر")]
+        public string sliderAlt { get; set; }
+
+        [Display(Name = "ترتیب اسلایدر")]
+        [Required(ErrorMessage = "لطفا {0} را وارد نمایید .")]
+        public int SliderSort { get; set; }
+
+        [Display(Name = "لینک اسلایدر")]
+        public string SliderLink { get; set; }
+
+        [Display(Name = "تصویر اسلایدر اسلایدر")]
+        public string SliderImg { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+}
---
+++ TshirtStudio.DataLayer/Migrations/20220527134632_Add_tbl_MainSlider.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TshirtStudio.DataLayer.Migrations
+{
+    public partial class Add_tbl_MainSlider : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "mainSliders",
+                columns: table => new
+                {
+                    Sliderid = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SliderTitle = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    sliderAlt = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SliderSort = table.Column<int>(type: "int", nullable: false),
+                    SliderLink = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SliderImg = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_mainSliders", x => x.Sliderid);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "mainSliders");
+        }
+    }
+}
