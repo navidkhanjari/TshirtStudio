e6c9851b52990321e79813c55a43027ddd99320a
navid khanjari <navidkhanjari72@gmail.com>
2022-07-18 14:38:30 +0430

    add Slider

--- TshirtStuido.Web/Areas/Admin/Controllers/SliderController.cs
+++ TshirtStuido.Web/Areas/Admin/Controllers/SliderController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using TshirtStudio.Core.Service.Interface;
+using TshirtStudio.DataLayer.Entities;
 
 namespace TshirtStuido.Web.Areas.Admin.Controllers
 {
@@ -18,6 +19,20 @@ namespace TshirtStuido.Web.Areas.Admin.Controllers
         public IActionResult Index()
         {
             return View(_sliderService.ShowAllSlider());
+        }
+        [HttpGet]
+        public IActionResult AddSlider()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddSlider(MainSlider mainslider)
+        {
+            if (!ModelState.IsValid)
+                return View(mainslider);
+            int res = _sliderService.AddSlider(mainslider);
+            TempData["Result"] = res > 0 ?"true" : "false";
+            return Redirect("~/Admin/Slider");
         }
     }
 }
